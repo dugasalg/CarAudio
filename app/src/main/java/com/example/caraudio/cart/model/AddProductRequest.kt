2dd27efe5560fd4e11f2aec7a5ab58f4404ea8ca
dugasalg <73446031+dugasalg@users.noreply.github.com>
2023-12-07 23:34:53 -0600

    crasheo todo ya no puedo

--- app/src/main/java/com/example/caraudio/cart/model/AddProductRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.caraudio.cart.model
-
-import com.example.caraudio.home.model.Products
-
-data class AddProductRequest(
-    val quotationId: String,
-    val product: Products
-)
---
+++ app/src/main/java/com/example/caraudio/cart/model/CarritoRequestBody.kt
@@ -0,0 +1,4 @@
+package com.example.caraudio.cart.model
+
+class CarritoRequestBody {
+}
